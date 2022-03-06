b2997fe2f0b06ffc7b581c3eaec62ef0a8e7a02b
wangjiaoj <2386467485@qq.com>
2022-03-06 16:12:02 +0800

    first commit

---
+++ use-vue3-ts/src/views/list/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    {{text}}
+    <Item v-for="(item,index) in list" :key="index" :data="item"></Item>
+  </div>
+</template>
+
+<script lang="ts">
+ 
+import {ref,defineComponent}from 'vue';
+import {useRouter, useRoute } from 'vue-router'
+import Item from './components/index.vue';
+export default defineComponent({
+  components:{
+  'Item':Item
+  },
+  setup(props) {
+      const router = useRouter()
+      const route = useRoute()
+      let text = ref("你好");
+      let list = ref([{title:"product001",detail:"my detail story"},
+      {title:"product001",detail:"my detail story"},
+      {title:"product001",detail:"my detail story"}])
+      return{
+        router,
+        route,
+        text,
+        list
+      }
+  }
+})
+</script>
