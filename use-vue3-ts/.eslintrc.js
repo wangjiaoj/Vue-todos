8ff3a382641039166cfe1dff371282825ec60c0e
wangjiaoj <2386467485@qq.com>
2022-04-14 20:47:11 +0800

    Update .eslintrc.js

--- use-vue3-ts/.eslintrc.js
+++ use-vue3-ts/.eslintrc.js
@@ -6,13 +6,19 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+     './.eslintrc-auto-import.json'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    'no-useless-escape': 'off',
+    "vue/multi-word-component-names": ["error", {
+        "ignores": ["index"] //需要忽略的组件名
+    }]
   }
 }
