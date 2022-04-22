71c4bdbd95df47969b4f30aac80ca3eb18ac4fd2
wangjiaoj <2386467485@qq.com>
2022-04-22 17:55:06 +0800

    Update index.vue

--- use-vue3-ts/src/components/popupMultiPicker/index.vue
+++ use-vue3-ts/src/components/popupMultiPicker/index.vue
@@ -4,13 +4,13 @@
     @click-overlay="close" 
     position="bottom"
   >
-      <div class="button-row">
+      <div class="multi-pop-header">
           <span  @click="onComfirm">确定</span>
           <span>{{title}}</span>
           <span  @click="close">取消</span>
       </div>
       <div class="group-list">
-        <van-checkbox-group :value="value">
+        <van-checkbox-group v-model="value">
           <van-cell-group>
             <van-cell
               v-for="(item) in options"
@@ -26,7 +26,7 @@
                 @click.stop
               />
           </template>
-              <!-- <van-checkbox :name="item.value" ref="checkboxes"  @click.stop/> -->
+      
             </van-cell>
           </van-cell-group>
         </van-checkbox-group>
@@ -40,7 +40,7 @@ export default {
   props: {
     showPicker: Boolean,
     title: String,
-    value: {
+    defValue: {
       type: Array,
       default() {
         return []
@@ -53,40 +53,37 @@ export default {
       }
     }
   },
-  // setup(){
-
-  // }
+  data(){
+     return{
+       value:[]
+     }
+  },
   methods: {
     close() {
       this.$emit('show', false);
     },
     toggle(index) {
-      let first = this.$refs.checkboxes[0];
-        console.log(`first.checked.value:${first.checked.value},toggle:${!first.checked.value}` )
-        first.toggle(!first.checked.value)
-        //first.toggle(!first.checked.value)
-      // console.log(this.$refs.checkboxes.find((item) => item.name === index).toggle)
       this.$refs.checkboxes.find((item) => item.name === index).toggle()
     },
     onComfirm(){
-      this.$refs.checkboxes.map(item=>{
-        console.log(` item.name:${ item.name}, item.value:${item.checked.value}`)
-      })
-      console.log( this.value)
-       
-      //  this.$emit('show', false);
+      // this.$refs.checkboxes.map(item=>{
+      //   console.log(` item.name:${ item.name}, item.value:${item.checked.value}`)
+      // })
+     
+       this.$emit('confirm', this.value);
     }
   },
   watch: {
-    result(newVal) {
-      console.log(newVal)
+    defValue(newData){
+       console.log('watch--defValue',newData)
+       this.value = newData
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.button-row{
+.multi-pop-header{
    padding-top:20px; 
    padding-bottom:20px;
    display:flex;
